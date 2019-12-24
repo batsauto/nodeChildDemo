55d811abc17cd6e6cf0fb0921a1a0df9c6600163
Christian Barrow <cfbarrow@gmail.com>
2019-12-24 12:29:55 -0600

    initial commit

---
+++ worker.js
@@ -0,0 +1,7 @@
+const { workerData, parentPort } = require('worker_threads')
+
+// You can do any heavy stuff here, in a synchronous way
+// without blocking the "main thread"
+
+console.log('Going to write tons of content on file '+workerData);
+parentPort.postMessage({ fileName: workerData, status: 'Done' })
